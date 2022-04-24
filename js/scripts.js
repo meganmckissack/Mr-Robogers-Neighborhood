d638fccdc706293ac46e4e8b6873b9aa9c22aa0b
Megan McKissack <meganmckissack@gmail.com>
2022-04-23 17:23:39 -0700

    create user input form and store value

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,34 @@
-const userNumArray = [];
-function numArray(usrNum) {
-  for (i = 0; i <= usrNum; i++) {
-    userNumArray.push(usrNum - [i]);
+$(document).ready(function(){
+  
+  $("#form").submit(function(event){
+    event.preventDefault();
+
+    const usrNumberInput = $("#usrNumber").val();
+    console.log(usrNumberInput);
+
+  const userNumArray = [];
+  function numArray(usrNum) {
+    for (i = 0; i <= usrNum; i++) {
+      userNumArray.push(usrNum - [i]);
+    }
+    return userNumArray.reverse();
   }
-  return userNumArray.reverse();
-}
-numArray(32)
+  numArray(usrNumberInput)
 
 
-function replaceNumbers() {
-const replacedNumArray = userNumArray.map(function(num, i){
-  if(num.toString().includes(1) && !num.toString().includes(2) && !num.toString().includes(3)){
-    return num[i] = "Bleep";
-  } else if(num.toString().includes(2) && !num.toString().includes(3)){
-    return num[i] = "Boop!"
-  } else if(num.toString().includes(3)) {
-    return num[i] = "Won't you be my neighbor?"
-  } else {
-    return num
+  function replaceNumbers() {
+    const replacedNumArray = userNumArray.map(function(num, i){
+      if(num.toString().includes(1) && !num.toString().includes(2) && !num.toString().includes(3)){
+        return num[i] = "Bleep";
+      } else if(num.toString().includes(2) && !num.toString().includes(3)){
+        return num[i] = "Boop!"
+      } else if(num.toString().includes(3)) {
+        return num[i] = "Won't you be my neighbor?"
+      } else {
+        return num
+      }
+    }); return replacedNumArray
   }
-}); return replacedNumArray
-}
-console.log(replaceNumbers())
+  console.log(replaceNumbers())  
+})
+})
